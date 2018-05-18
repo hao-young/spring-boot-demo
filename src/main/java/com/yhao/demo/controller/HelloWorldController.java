8a9b2180e7e645ca66d2501a91ec06a809494107
yhao <yhao2088@qq.com>
2018-05-18 13:18:52 +0800

    Spring boot 入门 单元测试 热启动设置

---
+++ src/main/java/com/yhao/demo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.yhao.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello world!";
+	}
+}
